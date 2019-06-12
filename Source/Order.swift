b5ed71f145175d55f95c9bebf8a625156bbbb40e
Kirtan Patel <kirtan@usebutton.com>
2019-06-12 17:24:42 -0400

    Update order with new properties

--- Source/Order.swift
+++ Source/Order.swift
@@ -43,8 +43,47 @@ final public class Order: NSObject, Codable {
     /**
      The ISO 4217 currency code (default is USD).
      */
-    private(set) var currencyCode: String
+    let currencyCode: String
+    
+    /**
+     The purchase date for the order.
+     */
+    let purchaseDate: Date?
+    
+    /**
+     The button source token
+     */
+    let sourceToken: String?
+    
+    /**
+     The customer-facing order id
+     */
+    let customerOrderId: String?
+    
+    /**
+     A list of the line item details that comprise the order
+     */
+    let lineItems: [LineItems]?
 
+    /**
+     The customer related to the order
+     */
+    let customer: Customer?
+    
+    /**
+     Represents a line item in the order
+     */
+    public class LineItems: NSObject, Codable {
+        
+    }
+    
+    /**
+     Represents a customer in the order
+     */
+    public class Customer: NSObject, Codable {
+        
+    }
+    
     /**
      Initializes an order object with the passed parameters.
 
@@ -58,12 +97,41 @@ final public class Order: NSObject, Codable {
         self.id = id
         self.amount = amount
         self.currencyCode = currencyCode
+    /**
+     Initializes an order object with the passed parameters.
+     
+     - Parameters:
+     - id: The order identifier (required).
+     - amount: The total order value in pennies or the
+               smallest decimal unit of the currency (e.g. 3999 for $39.99).
+               Deprecated
+     - currencyCode: The ISO 4217 currency code (default is USD).
+     - purchaseDate: The date of the purchase for the order.
+     - sourceToken: The customer-facing order identifer.
+     - customerOrderId: The customer-face order Id.
+     - lineItems: A list of the line item details that comprise the order.
+     - customer: Customer field
+     */
+    @objc public init(id: String, amount: Int64 = 0, currencyCode: String = "USD", purchaseDate: Date? = nil, sourceToken: String? = nil, customerOrderId: String? = nil, lineItems: [LineItems]? = nil, customer: Customer? = nil) {
+        self.id = id
+        self.amount = amount
+        self.currencyCode = currencyCode
+        self.purchaseDate = purchaseDate
+        self.sourceToken = sourceToken
+        self.customerOrderId = customerOrderId
+        self.lineItems = lineItems
+        self.customer = customer
     }
 
     enum CodingKeys: String, CodingKey {
         case id = "order_id"
         case amount
-        case currencyCode = "currency_code"
+        case currencyCode = "currency"
+        case purchaseDate = "purchase_date"
+        case sourceToken = "btn_ref"
+        case customerOrderId = "customer_order_id"
+        case lineItems = "line_items"
+        case customer = "customer"
     }
 
 }
